40845435662f5f76021752b675046ae7a6f47dfb
dinhsc <dinhc692@gmail.com>
2024-06-30 15:03:55 +0200

    introduce service

--- src/app/burgers/burgers.component.ts
+++ src/app/burgers/burgers.component.ts
@@ -1,28 +1,26 @@
-import { Component } from '@angular/core';
-import { Burger } from '../class/Burger';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BURGERS_DATA } from './data-burgers-list';
+import { BurgersService } from './burgers.service';
+import { Burger } from '../class/Burger';
 
 @Component({
   selector: 'app-burgers',
   templateUrl: './burgers.component.html',
   styleUrl: './burgers.component.css'
 })
-export class BurgersComponent {
+export class BurgersComponent implements OnInit {
+  
+  burgersList: Burger[] = [];
 
-  burgersList: Burger[] = [
-    { id: 0, name: "Le Big Mac", price: 10.5, description: "Un c...", imagePath: "../../assets/img/bic_mac.jpg" },
-    { id: 1, name: "Royal Deluxe", price: 8.75, description: "Pour ...", imagePath: "../../assets/img/deluxe.jpg" },
-    { id: 2, name: "Royal Bacon", price: 9.25, description: "Un b...", imagePath: "../../assets/img/bacon.jpg" },
-    { id: 3, name: "Tasty Bacon", price: 9.0, description: "Savo...", imagePath: "../../assets/img/tasty.jpg" },
-    { id: 4, name: "Royal Cheese", price: 8.5, description: "Le R...", imagePath: "../../assets/img/cheese.jpg" },
-    { id: 5, name: "Cheeseburger", price: 7.0, description: "Un b...", imagePath: "../../assets/img/cheeseburger.jpg" },
-    { id: 6, name: "McFish", price: 6.5, description: "Pour ...", imagePath: "../../assets/img/mcfish.jpg" },
-    { id: 7, name: "McCrispy", price: 8.0, description: "Décou...", imagePath: "../../assets/img/crispy.jpg" },
-  ]
+  constructor(
+    private router: Router,
+    private burgerService: BurgersService,
+  ) { }
 
-
-
-  constructor(private router: Router) { }
+  ngOnInit() {
+    this.burgersList = this.burgerService.getBurgerList();
+  }
   
   showDetails(id: number){
     const selectedBurger = this.burgersList.find(burger => burger.id == id);
--- src/app/detail-burger/detail-burger.component.ts
+++ src/app/detail-burger/detail-burger.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Burger } from '../class/Burger';
 import { ActivatedRoute } from '@angular/router';
+import { BURGERS_DATA } from '../burgers/data-burgers-list';
 
 @Component({
   selector: 'app-detail-burger',
@@ -9,16 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailBurgerComponent {
 
-  burgersList: Burger[] = [
-    { id: 0, name: "Big Mac", price: 10.5, description: "Le Big Mac™ est LE burger culte de McDonald’s, disponible dans de nombreux pays du monde ! Il a traversé les années et reste un indétrônable des burgers McDo™. Le secret de ce burger ? La simplicité de ses ingrédients, ses deux étages de saveurs, et bien entendu une sauce reconnaissable entre mille !", imagePath: "../../assets/img/bic_mac.jpg" },
-    { id: 1, name: "Royal Deluxe", price: 8.75, description: "Pour ...", imagePath: "../../assets/img/deluxe.jpg" },
-    { id: 2, name: "Royal Bacon", price: 9.25, description: "Un b...", imagePath: "../../assets/img/bacon.jpg" },
-    { id: 3, name: "Tasty Bacon", price: 9.0, description: "Savo...", imagePath: "../../assets/img/tasty.jpg" },
-    { id: 4, name: "Royal Cheese", price: 8.5, description: "Le R...", imagePath: "../../assets/img/cheese.jpg" },
-    { id: 5, name: "Cheeseburger", price: 7.0, description: "Un b...", imagePath: "../../assets/img/cheeseburger.jpg" },
-    { id: 6, name: "McFish", price: 6.5, description: "Pour ...", imagePath: "../../assets/img/mcfish.jpg" },
-    { id: 7, name: "McCrispy", price: 8.0, description: "Décou...", imagePath: "../../assets/img/crispy.jpg" },
-  ]
+  burgersList = BURGERS_DATA
   
   burger: Burger | undefined;
   isModalOpen: boolean = false
